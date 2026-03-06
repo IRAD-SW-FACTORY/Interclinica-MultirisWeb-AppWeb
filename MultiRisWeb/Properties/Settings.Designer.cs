f4a3891be0f1f8dab8ab6053f10ebcbd9b2e23fb
felipenarea <felipe.narea@irad.cl>
2026-03-06 16:21:37 -0300

    Pre-Merge Feature/audios con Main

---
+++ MultiRisWeb/Properties/Settings.Designer.cs
@@ -0,0 +1,143 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: MultiRisWeb.Properties.Settings
+// Assembly: MultiRisWeb, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 8E85F3B1-6D9E-40B3-B7C6-5E9DB57FC1C9
+// Assembly location: D:\Descompilacion9\MultirisWeb\Compilacion\bin\MultiRisWeb.dll
+
+using System.CodeDom.Compiler;
+using System.Configuration;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace MultiRisWeb.Properties
+{
+  [CompilerGenerated]
+  [GeneratedCode("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
+  internal sealed class Settings : ApplicationSettingsBase
+  {
+    private static Settings defaultInstance = (Settings) SettingsBase.Synchronized((SettingsBase) new Settings());
+
+    public static Settings Default => Settings.defaultInstance;
+
+    [ApplicationScopedSetting]
+    [DebuggerNonUserCode]
+    [DefaultSettingValue("http://testing.irad.cl/WSCHILLAN/ServiceMultiris.asmx")]
+    public string MultiRisWeb_WSMultirisChillan_ServiceMultiris => (string) this[nameof (MultiRisWeb_WSMultirisChillan_ServiceMultiris)];
+
+    [ApplicationScopedSetting]
+    [DebuggerNonUserCode]
+    [DefaultSettingValue("https://demo.irad.cl/wsmultiris/servicemultiris.asmx")]
+    public string MultiRisWeb_WSMRDemo_ServiceMultiris => (string) this[nameof (MultiRisWeb_WSMRDemo_ServiceMultiris)];
+
+    [ApplicationScopedSetting]
+    [DebuggerNonUserCode]
+    [DefaultSettingValue("https://risweb.ceimavan.cl/MultiRisWebWS/ServiceMultiris.asmx")]
+    public string MultiRisWeb_WSCeimavan_ServiceMultiris => (string) this[nameof (MultiRisWeb_WSCeimavan_ServiceMultiris)];
+
+    [ApplicationScopedSetting]
+    [DebuggerNonUserCode]
+    [DefaultSettingValue("https://pacientes.resomag.cl/MultiriswebWS/ServiceMultiris.asmx")]
+    public string MultiRisWeb_WSVitamedica_ServiceMultiris => (string) this[nameof (MultiRisWeb_WSVitamedica_ServiceMultiris)];
+
+    [ApplicationScopedSetting]
+    [DebuggerNonUserCode]
+    [DefaultSettingValue("https://risweb.resomag.cl/MultiriswebWS/ServiceMultiris.asmx")]
+    public string MultiRisWeb_WSHosmil_ServiceMultiris => (string) this[nameof (MultiRisWeb_WSHosmil_ServiceMultiris)];
+
+    [ApplicationScopedSetting]
+    [DebuggerNonUserCode]
+    [DefaultSettingValue("https://pacientes.resomag.cl/MultiRisWebWSMR/serviceMultiris.asmx")]
+    public string MultiRisWeb_WSVitalmedicaMR_ServiceMultiris => (string) this[nameof (MultiRisWeb_WSVitalmedicaMR_ServiceMultiris)];
+
+    [ApplicationScopedSetting]
+    [DebuggerNonUserCode]
+    [DefaultSettingValue("https://rishra.irad.cl/Multiriswebws/servicemultiris.asmx")]
+    public string MultiRisWeb_WSTalcaContingencia_ServiceMultiris => (string) this[nameof (MultiRisWeb_WSTalcaContingencia_ServiceMultiris)];
+
+    [ApplicationScopedSetting]
+    [DebuggerNonUserCode]
+    [DefaultSettingValue("https://riscuc.amis-rx.cl/MultiRisWebWS/ServiceMultiris.asmx")]
+    public string MultiRisWeb_WSCuc_ServiceMultiris => (string) this[nameof (MultiRisWeb_WSCuc_ServiceMultiris)];
+
+    [ApplicationScopedSetting]
+    [DebuggerNonUserCode]
+    [DefaultSettingValue("https://riscel.amis-rx.cl/MultiRisWebWS/ServiceMultiris.asmx")]
+    public string MultiRisWeb_WSLoa_ServiceMultiris => (string) this[nameof (MultiRisWeb_WSLoa_ServiceMultiris)];
+
+    [ApplicationScopedSetting]
+    [DebuggerNonUserCode]
+    [DefaultSettingValue("https://riscch.amis-rx.cl/MultiRisWebWS/ServiceMultiris.asmx")]
+    public string MultiRisWeb_WSChillan_ServiceMultiris => (string) this[nameof (MultiRisWeb_WSChillan_ServiceMultiris)];
+
+    [ApplicationScopedSetting]
+    [DebuggerNonUserCode]
+    [DefaultSettingValue("https://riscupm.amis-rx.cl/MultiRisWebWS/ServiceMultiris.asmx")]
+    public string MultiRisWeb_WSPuertoMontt_ServiceMultiris => (string) this[nameof (MultiRisWeb_WSPuertoMontt_ServiceMultiris)];
+
+    [ApplicationScopedSetting]
+    [DebuggerNonUserCode]
+    [DefaultSettingValue("https://rishra.irad.cl/MultiRisWebWSHRA/ServiceMultiris.asmx")]
+    public string MultiRisWeb_WSHRA_ServiceMultiris => (string) this[nameof (MultiRisWeb_WSHRA_ServiceMultiris)];
+
+    [ApplicationScopedSetting]
+    [DebuggerNonUserCode]
+    [DefaultSettingValue("http://10.0.10.2/MultiRisWebWSTRM/ServiceMultiris.asmx")]
+    public string MultiRisWeb_WSHosmet_ServiceMultiris => (string) this[nameof (MultiRisWeb_WSHosmet_ServiceMultiris)];
+
+    [ApplicationScopedSetting]
+    [DebuggerNonUserCode]
+    [DefaultSettingValue("https://imdu.irad.cl/MultiRisWebWS/ServiceMultiris.asmx")]
+    public string MultiRisWeb_WSImdu_ServiceMultiris => (string) this[nameof (MultiRisWeb_WSImdu_ServiceMultiris)];
+
+    [ApplicationScopedSetting]
+    [DebuggerNonUserCode]
+    [DefaultSettingValue("https://rishcua.amis-rx.cl/MultiRisWebWS/ServiceMultiris.asmx")]
+    public string MultiRisWeb_WSHCUA_ServiceMultiris => (string) this[nameof (MultiRisWeb_WSHCUA_ServiceMultiris)];
+
+    [ApplicationScopedSetting]
+    [DebuggerNonUserCode]
+    [DefaultSettingValue("https://ristalca.amis-rx.cl:1443/MultiriswebWS/ServiceMultiris.asmx")]
+    public string MultiRisWeb_WSASTalca_ServiceMultiris => (string) this[nameof (MultiRisWeb_WSASTalca_ServiceMultiris)];
+
+    [ApplicationScopedSetting]
+    [DebuggerNonUserCode]
+    [DefaultSettingValue("https://risla2.amis-rx.cl:1443/MultiriswebWS/ServiceMultiris.asmx")]
+    public string MultiRisWeb_WSLosAngeles_ServiceMultiris => (string) this[nameof (MultiRisWeb_WSLosAngeles_ServiceMultiris)];
+
+    [ApplicationScopedSetting]
+    [DebuggerNonUserCode]
+    [DefaultSettingValue("http://190.215.40.196/multiriswebws/servicemultiris.asmx")]
+    public string MultiRisWeb_WSIQQ_ServiceMultiris => (string) this[nameof (MultiRisWeb_WSIQQ_ServiceMultiris)];
+
+    [ApplicationScopedSetting]
+    [DebuggerNonUserCode]
+    [DefaultSettingValue("https://ancud.amis-rx.cl/multiriswebws/ServiceMultiris.asmx")]
+    public string MultiRisWeb_WSAsAncud_ServiceMultiris => (string) this[nameof (MultiRisWeb_WSAsAncud_ServiceMultiris)];
+
+    [ApplicationScopedSetting]
+    [DebuggerNonUserCode]
+    [DefaultSettingValue("http://10.0.10.10/multiriswebws/ServiceMultiris.asmx")]
+    public string MultiRisWeb_WsSanJose_ServiceMultiris => (string) this[nameof (MultiRisWeb_WsSanJose_ServiceMultiris)];
+
+    [ApplicationScopedSetting]
+    [DebuggerNonUserCode]
+    [DefaultSettingValue("https://rishra.irad.cl/Multiriswebws/servicemultiris.asmx")]
+    public string MultiRisWeb_WSSoranWeb_ServiceMultiris => (string) this[nameof (MultiRisWeb_WSSoranWeb_ServiceMultiris)];
+
+    [ApplicationScopedSetting]
+    [DebuggerNonUserCode]
+    [DefaultSettingValue("http://risweb.irad.cl/WSObtenertecnologo/ServiceSoran_Tecnologo.asmx")]
+    public string MultiRisWeb_WS_Codelco_Tecnologo_ServiceSoran_Tecnologo => (string) this[nameof (MultiRisWeb_WS_Codelco_Tecnologo_ServiceSoran_Tecnologo)];
+
+    [ApplicationScopedSetting]
+    [DebuggerNonUserCode]
+    [DefaultSettingValue("https://hnaval.irad.cl/Multiriswebws/servicemultiris.asmx")]
+    public string MultiRisWeb_WsHospNavTalcahuano_ServiceMultiris => (string) this[nameof (MultiRisWeb_WsHospNavTalcahuano_ServiceMultiris)];
+
+    [ApplicationScopedSetting]
+    [DebuggerNonUserCode]
+    [SpecialSetting(SpecialSetting.WebServiceUrl)]
+    [DefaultSettingValue("http://10.0.10.2/MultiRisWebWSCodelco/ServiceMultiris.asmx")]
+    public string MultiRisWeb_WSCodelcoHosting_ServiceMultiris => (string) this[nameof (MultiRisWeb_WSCodelcoHosting_ServiceMultiris)];
+  }
+}
